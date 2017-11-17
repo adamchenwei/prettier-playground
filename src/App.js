a0c548a27d81411e4cbfaad78174021312da1168
Adam Chen Wei <awei@pebblepost.com>
2017-11-17 14:02:31 -0500

    feat(config): more prettier config added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   prettierTestingCode() {
-    function anotherTesterFunc(
-      ar1,
-      arg2,
-      arg3,
-      arg4,
-      arg5
-    ) {}
+    function anotherTesterFunc(ar1, arg2, arg3, arg4, arg5) {}
+    function hey() {}
   }
 
   anotherThing() {}
@@ -20,28 +13,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img
-            src={
-              logo
-            }
-            className="App-logo"
-            alt="logo"
-          />
-          <h1 className="App-title">
-            Welcome
-            to React
-          </h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          AAAAA To
-          get
-          started,
-          edit{" "}
-          <code>
-            src/App.js
-          </code>{" "}
-          and save
-          to reload.
+          AAAAA To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
